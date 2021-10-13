468380df61241b9118819eaaaeb4d7eb08c4cffa
rikardseg <rikard.segerkvist@hotmail.com>
2021-10-13 11:35:20 +0200

    media query

--- html/wp-content/themes/steinertheme/hamburger.js
+++ html/wp-content/themes/steinertheme/hamburger.js
@@ -1,6 +1,16 @@
 console.log("LOLL");
 
 function hamburgerToggle() {
-  const hamburgerMenu = document.querySelector(".dropdown");
+  const dropDown = document.querySelector(".dropdown");
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+  dropDown.classList.toggle("on");
   hamburgerMenu.classList.toggle("on");
+  console.log("hello");
 }
+
+const hamburgerButton = document.querySelector("button");
+const dropdownButton = document.querySelector(".dropdown");
+
+hamburgerButton.addEventListener("click", hamburgerToggle);
+dropdownButton.addEventListener("click", hamburgerToggle);
