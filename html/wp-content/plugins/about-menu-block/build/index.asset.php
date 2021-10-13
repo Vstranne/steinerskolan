3361020f863ba6ad70d240b63adab2260310cd01
rikardseg <rikard.segerkvist@hotmail.com>
2021-10-14 01:03:13 +0200

    about menu block

--- html/wp-content/plugins/about-menu-block/build/index.asset.php
+++ html/wp-content/plugins/about-menu-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '07823a74e3acf81dd861b654f4109ccb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => 'eccfebca343f8acef9b453a9ec180c2e');
